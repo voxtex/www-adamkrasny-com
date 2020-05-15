bcc903cf7940835478d76825ad4982902a941db7
Adam Krasny <voxtex@gmail.com>
2020-05-14 19:01:25 -0700

    add fallback background color, better background size on mobile, update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,23 @@ const Index = (): React.ReactElement => (
     <h1 className="mt-0">Welcome</h1>
     <p>This is my site, a catalog of my experience and experiments.</p>
     <p>
-      I&apos;m a web, desktop, and mobile developer with over 10 years of software engineering experience. My current
-      focus is full-stack TypeScript and JavaScript.
+      I&apos;m a web, desktop, and mobile developer with over 10 years of professional software engineering experience.
+      My current focus is full-stack TypeScript and JavaScript.
     </p>
-    <a href="http://stackoverflow.com/users/2639178/adamk" target="_blank" rel="noopener noreferrer" className="mr-4">
-      Stack Overflow
-    </a>
-    <a href="http://www.linkedin.com/in/adamkrasny" target="_blank" rel="noopener noreferrer" className="mr-4">
-      LinkedIn
-    </a>
-    <a href="https://github.com/voxtex" target="_blank" rel="noopener noreferrer" className="mr-4">
-      GitHub
-    </a>
-    <a href="mailto:adam@adamkrasny.com" target="_blank" rel="noopener noreferrer">
-      Email
-    </a>
+    <p>
+      <a href="http://stackoverflow.com/users/2639178/adamk" target="_blank" rel="noopener noreferrer" className="mr-4">
+        Stack Overflow
+      </a>
+      <a href="http://www.linkedin.com/in/adamkrasny" target="_blank" rel="noopener noreferrer" className="mr-4">
+        LinkedIn
+      </a>
+      <a href="https://github.com/voxtex" target="_blank" rel="noopener noreferrer" className="mr-4">
+        GitHub
+      </a>
+      <a href="mailto:adam@adamkrasny.com" target="_blank" rel="noopener noreferrer">
+        Email
+      </a>
+    </p>
   </div>
 );
 
