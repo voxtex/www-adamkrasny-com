e3f167a23c0fcedaf366f79781e499a1882a33e3
Adam Krasny <voxtex@gmail.com>
2020-05-15 22:07:11 -0700

    make favicon urls absolute

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,11 +25,11 @@ const MyApp = ({ Component, pageProps }: Props): React.ReactElement => {
         <title>Adam Krasny</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="favicon/favicon.ico?v=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-        <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="icon" href="/favicon/favicon.ico?v=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <nav className="bg-black bg-opacity-75 px-4 text-white text-center pb-0 md:text-left md:px-6 md:bg-opacity-50">
         <Link href="/">
